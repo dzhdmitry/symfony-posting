c9062088e468cecd799e69a2ecc76266dd266170
Dmitry <dzhdmitry@gmail.com>
2017-04-23 15:07:19 +0300

    Styles fixed

--- app/Resources/public/js/script.js
+++ app/Resources/public/js/script.js
@@ -1,21 +1,28 @@
 $(function() {
+    var $btn = $('a.action-add-tag'),
+        max = $btn.data('max'),
+        $tags = $($btn.data('target'));
+
+    function maxIsReached() {
+        return (max !== undefined) && ($tags.find('div.tag-row').length >= max);
+    }
+
+    function toggleButton() {
+        $btn.toggleClass('disabled', maxIsReached());
+    }
+
     $('div.tags-container').each(function(i, container) {
         var $container = $(container);
 
-        $container.data('index', $container.find('div.tag-row').length)
+        $container.data('index', $container.find('div.tag-row').length);
+        toggleButton();
     });
 
-    $('a.action-add-tag').click(function(e) {
+    $btn.click(function(e) {
         e.preventDefault();
 
-        var $this = $(this),
-            max = $this.data('max'),
-            $tags = $($this.data('target'));
-
-        if (max !== undefined) {
-            if ($tags.find('div.tag-row').length >= max) {
-                return;
-            }
+        if (maxIsReached()) {
+            return;
         }
 
         var prototype = $tags.data('prototype'),
@@ -23,11 +30,13 @@ $(function() {
             tagForm = prototype.replace(/__name__/g, index);
 
         $tags.data('index', index + 1).append(tagForm);
+        toggleButton();
     });
 
     $(document).on('click', 'button.action-remove-tag', function(e) {
         e.preventDefault();
 
         $(this).closest('div.tag-row').remove();
+        toggleButton();
     });
 });
